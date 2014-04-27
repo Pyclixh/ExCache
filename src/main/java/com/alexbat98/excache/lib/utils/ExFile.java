f322fd47424b17ec14bc3fff521b593556b006a6
Alexander Batashev <alexbatashev@gmail.com>
2014-04-27 20:12:58 +0400

    Minor changes

--- src/main/java/com/alexbat98/excache/lib/utils/ExFile.java
+++ src/main/java/com/alexbat98/excache/lib/utils/ExFile.java
@@ -4,7 +4,6 @@ package com.alexbat98.excache.lib.utils;
  * Created by Alexander Batashev on 27.04.2014.
  */
 public interface ExFile {
-    public int getType();
     public void setData();
     public Object getData();
 }
--- src/main/java/com/alexbat98/excache/lib/utils/ExImageFile.java
+++ src/main/java/com/alexbat98/excache/lib/utils/ExImageFile.java
@@ -6,12 +6,6 @@ import android.graphics.Bitmap;
  * Created by Alexander Batashev on 27.04.2014.
  */
 public class ExImageFile implements ExFile {
-
-    @Override
-    public int getType() {
-        return 1;
-    }
-
     @Override
     public void setData() {
 
